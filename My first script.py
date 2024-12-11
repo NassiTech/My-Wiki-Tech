7a23a91e8e8bbb3f2e8422d57951bb9bfb703165
Nassima.amroun <nassima.amroun@tn.techstartere.de>
2024-12-11 14:19:07 +0100

    Create My first script.py

---
+++ My first script.py
@@ -0,0 +1,9 @@
+script.py
+first_name = "Nassima"
+last_name = "A."
+print(first_name + " " + last_name)
+number1 = 3
+number2 = 15
+print("number1"+ "number2:", Number1 + number2)
+print("number1* number2:" , number1 * number2)
+Print("number2/number1" , number2 / number1)
